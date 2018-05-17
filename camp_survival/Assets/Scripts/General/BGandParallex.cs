92fc76c1ba7a93ed61beec75a3550739c6a6aa55
Kiviloo <spoxe_77@web.de>
2018-05-17 21:01:38 +0200

    Base v0.2
    Basis für das Spiel.
    Incl. TestLevel

---
+++ camp_survival/Assets/Scripts/General/BGandParallex.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGandParallex : MonoBehaviour {
+
+/*	public float backgroundSize;
+
+	private int count;
+	private Transform cameraTransform;
+	public Transform[] bg;
+	public Transform[] clouds;
+	public Transform[] sky;
+	public SpriteRenderer sprite;
+
+	private int leftIndex;
+	private int rightIndex;
+
+	void Start () {
+		
+		leftIndex = 0;
+		rightIndex = 2;
+		sprite = GetComponent<SpriteRenderer> ();
+		Left ();
+	}
+
+	private void Left() {
+
+		Vector3 deltaLeft = new Vector3 (sprite.bounds.size.x, 0);
+
+		bg [rightIndex].position = bg[leftIndex].position - deltaLeft;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		cameraTransform = Camera.current.transform;
+
+	}
+*/
+}
