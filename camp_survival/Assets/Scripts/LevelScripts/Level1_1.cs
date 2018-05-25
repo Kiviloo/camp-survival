5c2357761856f9fdeef7852da34051794ec4c7ad
Kiviloo <spoxe_77@web.de>
2018-05-25 20:59:56 +0200

    v0.2.3
    Bonny
    Maya

---
+++ camp_survival/Assets/Scripts/LevelScripts/Level1_1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1_1 : MonoBehaviour {
+
+
+	void Start () {
+
+		StartCoroutine (HoldOn ());
+	}
+	
+	IEnumerator HoldOn() {
+
+		yield return new WaitForSeconds (10);
+		SceneManager.LoadScene ("1.2", LoadSceneMode.Single);
+	}
+}
