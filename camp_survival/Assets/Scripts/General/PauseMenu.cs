8113e9df5d4e5f0cb593cf88458ac470d2fccf83
SpeedyPlays <38811018+SpeedyPlays@users.noreply.github.com>
2018-05-27 02:08:03 +0200

    A3.2.2A V0.2

--- camp_survival/Assets/Scripts/General/PauseMenu.cs
+++ camp_survival/Assets/Scripts/General/PauseMenu.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private Transform[] voice;
+	private bool paused = false;
+	private int isPlaying;
+
+
+	void Start() {
+
+		GameObject.Find ("PauseMenu").GetComponent<Canvas> ().enabled = false;
 	}
-	
-	// Update is called once per frame
+
+
 	void Update () {
-		
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			if (paused == false) {
+
+				Pause ();
+			} else {
+
+				Resume ();
+			}
+		}
+	}
+
+
+	public void Pause() {
+
+		GameObject.Find ("PauseMenu").GetComponent<Canvas> ().enabled = true;
+		Time.timeScale = 0f;
+		paused = true;
+
+		AudioSource audio = GameObject.FindGameObjectWithTag ("Voice").GetComponent<AudioSource> ();
+
+		audio.Pause ();
+	}
+
+
+	public void Resume() {
+
+		GameObject.Find ("PauseMenu").GetComponent<Canvas> ().enabled = false;
+		Time.timeScale = 1f;
+		paused = false;
+
+		AudioSource audio = GameObject.FindGameObjectWithTag ("Voice").GetComponent<AudioSource> ();
+
+		audio.UnPause ();
 	}
 }
---
+++ camp_survival/Assets/Scripts/General/Voice.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voice : MonoBehaviour {
+
+	public AudioClip[] voiceOver;
+	private AudioClip[] voices;
+	public float[] seconds;
+	public AudioSource audio;
+
+	void Start() {
+
+		StartCoroutine(PlaySound ());
+	}
+
+	IEnumerator PlaySound() {
+
+		voices = new AudioClip[voiceOver.Length];
+
+		AudioSource audio = GameObject.FindGameObjectWithTag ("Voice").GetComponent<AudioSource> ();
+
+		for (int i = 0; i <= voiceOver.Length - 1; i++) {
+
+			voices [i] = voiceOver [i];
+		}
+
+		for (int i = 0; i <= voiceOver.Length - 1; i++) {
+
+			audio.clip = voices [i];
+			audio.Play ();
+
+			if (i > seconds.Length - 1) {
+				yield return new WaitForSeconds (audio.clip.length);
+			} else {
+				yield return new WaitForSeconds (audio.clip.length + seconds [i]);
+			}
+		}
+	}
+}
