5ba04a945fd2fcb287e826b258ba740d5f0934f1
Kiviloo <spoxe_77@web.de>
2018-06-02 21:35:21 +0200

    v0.3

---
+++ camp_survival/Assets/BulletTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTime : MonoBehaviour {
+
+	void Start () {
+
+		StartCoroutine (Beginn ());
+		}
+
+	IEnumerator Beginn() {
+
+		yield return new WaitForSeconds (6);
+		Time.timeScale = 0.1f;
+		yield return new WaitForSeconds (2);
+		Time.timeScale = 1f;
+	}
+}
